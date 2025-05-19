9d9ff6c42dddcfb7b76a2c60dc9772c97ad850f9
Yakir Gagnon <12.yakir@gmail.com>
2025-05-19 15:38:09 +0200

    change port

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ async fn hello_world() -> &'static str {
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     let router = Router::new().route("/", get(hello_world));
-    let addr = SocketAddr::from(([127,0,0,1], 8000));
+    let addr = SocketAddr::from(([0,0,0,0], 8080));
     let tcp = TcpListener::bind(&addr).await.unwrap();
     axum::serve(tcp, router).await.unwrap();
 }
